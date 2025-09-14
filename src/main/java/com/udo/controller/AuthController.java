d3a89c62666b5744273b7016aa8bfe4a9110d7eb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-14 20:47:40 +0700

    Sign in and Login

--- src/main/java/com/udo/controller/AuthController.java
+++ src/main/java/com/udo/controller/AuthController.java
@@ -1,6 +1,13 @@
 package com.udo.controller;
 
+import com.udo.exceptions.UserException;
+import com.udo.payload.DTO.UserDTO;
+import com.udo.payload.response.AuthResponse;
+import com.udo.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signupHandler(@RequestBody UserDTO userDTO) throws UserException {
+        return ResponseEntity.ok(authService.signup(userDTO));
+    }
 }
