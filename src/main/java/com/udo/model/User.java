87a792acd0a1b5337b36c15d551be7446134b282
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-15 08:18:13 +0700

    Sign in and Login

--- src/main/java/com/udo/model/User.java
+++ src/main/java/com/udo/model/User.java
@@ -1,10 +1,7 @@
 package com.udo.model;
 
 import com.udo.domain.UserRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 
@@ -19,6 +16,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
