a75e4849c627ce5b3783174599e929b02c872de7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-16 20:43:34 +0800

    Sign in and Login

---
+++ src/main/java/com/udo/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.udo.service.impl;
+
+import com.udo.configuration.JwtProvider;
+import com.udo.exceptions.UserException;
+import com.udo.model.User;
+import com.udo.repository.UserRepository;
+import com.udo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public User getUserFromJwtToken(String token) throws UserException {
+        String email = jwtProvider.getEmailFromJwtToken(token);
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UserException("Invalid token");
+        }
+        return user;
+    }
+
+    @Override
+    public User getCurrentUser() throws UserException {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UserException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User getUserByEmail(String email) throws UserException {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UserException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
