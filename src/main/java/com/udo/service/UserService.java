375818d2b53ad8d44f5b4cc83fdfd140b5e465ce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-16 20:43:30 +0800

    Sign in and Login

---
+++ src/main/java/com/udo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.udo.service;
+
+import com.udo.exceptions.UserException;
+import com.udo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User getUserFromJwtToken(String token) throws UserException;
+    User getCurrentUser() throws UserException;
+    User getUserByEmail(String email) throws UserException;
+    User getUserById(Long id);
+    List<User> getAllUsers();
+}
