f869af438c2be1bc0fa9eb8791711a7afda18829
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-18 08:31:26 +0800

    Sign in and Login

---
+++ src/main/java/com/udo/model/StoreContact.java
@@ -0,0 +1,15 @@
+package com.udo.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class StoreContact {
+    private String address;
+    private String phone;
+
+    @Email(message = "Invalid email format")
+    private String email;
+}
