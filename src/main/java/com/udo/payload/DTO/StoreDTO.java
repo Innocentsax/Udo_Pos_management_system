dae2bc909c0a64ef2be0511370eb0e0a56c482b8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-21 21:15:07 +0800

    Store onboarding setup

--- src/main/java/com/udo/payload/DTO/StoreDTO.java
+++ src/main/java/com/udo/payload/DTO/StoreDTO.java
@@ -3,9 +3,11 @@ package com.udo.payload.DTO;
 import com.udo.domain.StoreStatus;
 import com.udo.model.StoreContact;
 import com.udo.model.User;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class StoreDTO {
     private Long id;
     private String brand;
