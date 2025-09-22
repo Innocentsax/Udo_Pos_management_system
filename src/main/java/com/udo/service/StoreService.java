ff37f8a71bdc56680d027d486cdebc22e88a1776
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-22 22:31:22 +0800

    Store management set up

--- src/main/java/com/udo/service/StoreService.java
+++ src/main/java/com/udo/service/StoreService.java
@@ -1,4 +1,12 @@
 package com.udo.service;
 
-public class StoreService {
+import com.udo.model.User;
+import com.udo.payload.DTO.StoreDTO;
+
+import java.util.List;
+
+public interface StoreService {
+    StoreDTO createStore(StoreDTO storeDTO, User user);
+    StoreDTO getStoreById(Long id);
+    List<StoreDTO> getAllStore();
 }
