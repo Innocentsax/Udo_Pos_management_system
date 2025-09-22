61bb99f063f941f9c02adaf9bd0db707e95a7f2e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-23 07:28:18 +0800

    Sign in and Login

--- src/main/java/com/udo/service/StoreService.java
+++ src/main/java/com/udo/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.udo.service;
 
+import com.udo.model.Store;
 import com.udo.model.User;
 import com.udo.payload.DTO.StoreDTO;
 
@@ -9,4 +10,8 @@ public interface StoreService {
     StoreDTO createStore(StoreDTO storeDTO, User user);
     StoreDTO getStoreById(Long id);
     List<StoreDTO> getAllStore();
+    Store getStoreByAdmin();
+    StoreDTO updateStore(Long id, StoreDTO storeDTO);
+    StoreDTO deleteStore(Long id);
+    StoreDTO getStoreByEmployee();
 }
