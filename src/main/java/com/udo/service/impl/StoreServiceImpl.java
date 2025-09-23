419387de0601b3a7188bb7b921021c684ffb7f5c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-23 18:56:42 +0800

    Sign in and Login

---
+++ src/main/java/com/udo/service/impl/StoreServiceImpl.java
@@ -0,0 +1,55 @@
+package com.udo.service.impl;
+
+import com.udo.model.Store;
+import com.udo.model.User;
+import com.udo.payload.DTO.StoreDTO;
+import com.udo.repository.StoreRepository;
+import com.udo.service.StoreService;
+import com.udo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StoreServiceImpl implements StoreService {
+
+    private final StoreRepository storeRepository;
+    private final UserService userService;
+
+    @Override
+    public StoreDTO createStore(StoreDTO storeDTO, User user) {
+        return null;
+    }
+
+    @Override
+    public StoreDTO getStoreById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<StoreDTO> getAllStore() {
+        return List.of();
+    }
+
+    @Override
+    public Store getStoreByAdmin() {
+        return null;
+    }
+
+    @Override
+    public StoreDTO updateStore(Long id, StoreDTO storeDTO) {
+        return null;
+    }
+
+    @Override
+    public StoreDTO deleteStore(Long id) {
+        return null;
+    }
+
+    @Override
+    public StoreDTO getStoreByEmployee() {
+        return null;
+    }
+}
