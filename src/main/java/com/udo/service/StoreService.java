fb1e80fa4511453346a09f1031f5c9b950aab508
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-25 01:18:27 +0800

    Store management setup

--- src/main/java/com/udo/service/StoreService.java
+++ src/main/java/com/udo/service/StoreService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface StoreService {
     StoreDTO createStore(StoreDTO storeDTO, User user);
-    StoreDTO getStoreById(Long id);
+    StoreDTO getStoreById(Long id) throws Exception;
     List<StoreDTO> getAllStore();
     Store getStoreByAdmin();
     StoreDTO updateStore(Long id, StoreDTO storeDTO);
