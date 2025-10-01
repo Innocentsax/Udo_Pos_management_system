d82b11b1642ce3936ef07c670b474e601a8b3132
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-01 23:20:59 +0800

    Store management setup

--- src/main/java/com/udo/controller/StoreController.java
+++ src/main/java/com/udo/controller/StoreController.java
@@ -1,10 +1,13 @@
 package com.udo.controller;
 
+import com.udo.exceptions.UserException;
+import com.udo.model.User;
+import com.udo.payload.DTO.StoreDTO;
 import com.udo.service.StoreService;
 import com.udo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreController {
     private final StoreService storeService;
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<StoreDTO> createStore(@RequestBody StoreDTO storeDTO,
+                                                @RequestHeader("Authorization") String jwt) throws UserException {
+        User user = userService.getUserFromJwtToken(jwt);
+        return ResponseEntity.ok(storeService.createStore(storeDTO, user));
+    }
 }
